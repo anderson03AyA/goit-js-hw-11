f5a1c58319f91678a0074b288fd4c24f38c3a5c7
anderson03AyA <andersonsolarte03@gmail.com>
2023-04-28 01:03:50 -0500

    complete

--- src/index.js
+++ src/index.js
@@ -1,95 +1,111 @@
 import './css/styles.css';
 import { Notify } from 'notiflix';
+import { myValue } from './key';
+import $ from 'jquery';
+import axios from 'axios';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 
-const DEBOUNCE_DELAY = 300;
+const div = document.querySelector('div');
+const searchInput = document.querySelector('.search');
+const send = document.querySelector('.send');
+const btnBack = document.querySelector('.btnBack');
+const btnNext = document.querySelector('.btnNext');
+const lightbox = new SimpleLightbox('.gallery a');
 
-const inputText = document.getElementById('search-box');
-const ul = document.querySelector('.country-list');
-const div = document.querySelector('.country-info');
-
-const API_URL = `https://restcountries.com/v3.1/name/`;
-
-const debounce = (callback, delay) => {
-  let timer;
-  return (...args) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => callback(...args), delay);
-  };
+const renderPhotoCard = (img, likes, views, comments, downloads) => {
+  return `<div class="photo-card">
+    <a href="${img}" class="gallery"><img src="${img}" alt="" loading="lazy" width="100px" heigth="100px" /></a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes: </b>
+        <strong> ${likes} </strong>
+      </p>
+      <p class="info-item">
+        <b>Views: </b>
+        <strong> ${views} </strong>
+      </p>
+      <p class="info-item">
+        <b>Comments: </b>
+        <strong> ${comments} </strong>
+      </p>
+      <p class="info-item">
+        <b>Downloads: </b>
+        <strong> ${downloads} </strong>
+      </p>
+    </div>
+  </div>`;
 };
 
-const handleInput = event => {
-  if (inputText.value === '') {
-    ul.style.display = 'none';
-    div.style.display = 'none';
-  } else {
-    fetch(`${API_URL}${inputText.value.trim()}`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Oops, there is no country with that name');
-        }
-        return response.json();
-      })
-      .then(users => {
-        if (users.length >= 2 && users.length <= 10) {
-          const tpl = users.map(
-            user =>
-              `<li><img src="${user.flags.svg}" alt="Flag of ${user.name.common}" width="50" height="30">${user.name.common}</li>`
-          );
-          ul.style.display = 'flex';
-          div.style.display = 'none'
-          ul.innerHTML = `<ul>${tpl}</ul>`;
-            
-        }
-        else if (users.length > 10) {
-          Notify.info(
-            `"Too many matches found. Please enter a more specific name."`
-          );
-          ul.style.display = 'none';
-          div.style.display = 'none';
-        }
-        else if (users.length === 1) {
-          const tpl = users.map(
-            user =>
-              `
-                <div class="list"> 
-                <img src="${user.flags.svg}"
-                 alt="Flag of ${user.name.common}" 
-                 width="50" height="30">${user.name.common}
-                 </div>
-                 <div class="list"> <br>
-                 <h6>Capital: </h6>   ${user.capital} 
-                 </div>
-                 <div class="list"> 
-                 <h6>Population: </h6>   ${user.population} 
-                 </div>
-                 <div class="list"> 
-                 <h6>Languages: </h6>   ${Object.values(user.languages).join(
-                ', '
-              )}
-                 </div> `
-            //languages es un objeto por lo que pueden haber varios idiomas
-          );
-          div.style.display = 'flex';
-          ul.style.display = 'none';
-          div.innerHTML = `${tpl}`;
-        }
-          
-          
-      }).catch(error => {
-        Notify.failure(error)
-          ul.style.display = 'none';
-          div.style.display = 'none';
+const API_KEY = myValue;
+
+// Páginas a recuperar
+let page = 1;
+let per_page = 20;
+let page_actual = page * per_page;
+let total_page;
+
+const results = async () => {
+  const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${searchInput.value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}`;
+  try {
+    const response = await axios.get(URL);
+    const { data } = response;
+    if (parseInt(data.totalHits) > 0) {
+      total_page = data.totalHits;
+      const photoCards = data.hits.map(hit => {
+        return renderPhotoCard(
+          hit.largeImageURL,
+          hit.likes,
+          hit.views,
+          hit.comments,
+          hit.downloads
+        );
       });
+      div.innerHTML = photoCards.join('');
+      lightbox.refresh();
+
+      if (page === 1) {
+        btnBack.style.display = 'flex';
+        btnNext.style.display = 'flex';
+      }
+    } else {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+  } catch (error) {
+    Notify.failure('error');
   }
 };
 
-const debouncedHandleInput = debounce(handleInput, DEBOUNCE_DELAY);
-inputText.addEventListener('input', debouncedHandleInput);
+send.addEventListener('click', e => {
+  e.preventDefault();
+  if (searchInput.value === '') {
+    Notify.warning('input invalid');
+  } else {
+    results();
+  }
+});
 
-const numberChildValid = numberOfChildren => {
-  console.log(numberOfChildren);
-  if (numberOfChildren > 0) {
-    console.log(numberOfChildren);
-    return true;
-  } else return false;
-};
+btnBack.addEventListener('click', () => {
+  if (page === 1) {
+    Notify.warning('is in the first');
+  } else {
+    page -= 1;
+    results();
+  }
+});
+
+btnNext.addEventListener('click', () => {
+  if (page_actual >= total_page) {
+    Notify.failure('Hooray! We found totalHits images.');
+  } else {
+    page += 1;
+    results();
+    page_actual = per_page * page;
+
+    setTimeout(() => {
+      document.querySelector('div').scrollIntoView({ behavior: 'smooth' });
+    }, 2000);
+  }
+});
---
+++ src/key.js
@@ -0,0 +1,1 @@
+export const myValue = '35744314-d7918009afa52f8a17d2f0f98';
